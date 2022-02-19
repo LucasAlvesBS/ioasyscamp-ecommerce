72f399a8fec75004fd396e6dd101e700a89a2474
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-19 20:19:07 -0300

    feat(helpers): remove percentage property

--- src/helpers/regex.helper.ts
+++ src/helpers/regex.helper.ts
@@ -3,14 +3,10 @@ const password =
 
 const name = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
 
-const username = /^[a-zA-Z0-9]*$/;
-
 const cpf = /([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
 
 const telephone =
   /^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$/;
-
-const percentage = /^\d{1,3}\%$/;
 
 const zipCode = /^[0-9]{8}$/;
 
@@ -19,7 +15,5 @@ export const RegExHelper = {
   name,
   cpf,
   telephone,
-  username,
-  percentage,
   zipCode,
 };

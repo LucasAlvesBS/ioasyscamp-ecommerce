c76c12c1f837ff079852bcec9db677f8eaa35b5b
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-19 20:18:08 -0300

    feat(migrations): updates relationships with other tables

--- src/config/typeorm/migrations/1645232300267-CreateUser.ts
+++ src/config/typeorm/migrations/1645232300267-CreateUser.ts
@@ -3,9 +3,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateUser1645232300267 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        CREATE TYPE role_type AS ENUM ('user', 'admin');
+        CREATE TYPE role_options AS ENUM ('user', 'admin');
         CREATE TABLE users (
-        id varchar(255) PRIMARY KEY,
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         first_name varchar(100) NOT NULL,
         last_name varchar(100) NOT NULL,
         email varchar(255) NOT NULL UNIQUE,
@@ -13,7 +13,7 @@ export class CreateUser1645232300267 implements MigrationInterface {
         cpf varchar(11) NOT NULL UNIQUE,
         telephone varchar(255) NOT NULL,
         gender varchar(100),
-        role role_type,
+        role role_options DEFAULT 'user',
         created_at timestamp DEFAULT CURRENT_TIMESTAMP,
         updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
         deleted_at timestamp 
@@ -21,6 +21,6 @@ export class CreateUser1645232300267 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('DROP TABLE users; DROP TYPE role_type');
+    await queryRunner.query('DROP TABLE users; DROP TYPE role_options');
   }
 }
--- src/config/typeorm/migrations/1645236411166-CreateAddress.ts
+++ src/config/typeorm/migrations/1645236411166-CreateAddress.ts
@@ -4,13 +4,13 @@ export class CreateAddress1645236411166 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     CREATE TABLE addresses (
-        id varchar(255) PRIMARY KEY,
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         state varchar(50) NOT NULL,
         city varchar(50) NOT NULL,
         address1 varchar(255) NOT NULL UNIQUE,
         address2 varchar(255),
         zip_code varchar(8) NOT NULL,
-        user_id varchar(255) REFERENCES users(id),
+        user_id UUID REFERENCES users(id),
         created_at timestamp DEFAULT CURRENT_TIMESTAMP,
         updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
         deleted_at timestamp 

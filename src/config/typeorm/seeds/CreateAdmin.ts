845dade5594d429b4130f35dea7aab7c3fbd5e76
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-20 12:38:29 -0300

    feat(seeds): add new seeds to insert some records

---
+++ src/config/typeorm/seeds/CreateAdmin.ts
@@ -0,0 +1,30 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+import { UsersEntity } from '../../../app/users/users.entity';
+import { hashSync } from 'bcrypt';
+import { Role } from '../../enum/role.enum';
+
+const adminPassword = process.env.ADMIN_PASSWORD;
+const hashedPassword = hashSync(adminPassword, 10);
+
+export class CreateAdmin implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(UsersEntity)
+      .values([
+        {
+          firstName: process.env.ADMIN_FIRST_NAME,
+          lastName: process.env.ADMIN_LAST_NAME,
+          email: process.env.ADMIN_EMAIL,
+          password: `${hashedPassword}`,
+          cpf: process.env.ADMIN_CPF,
+          telephone: process.env.ADMIN_TELEPHONE,
+          gender: process.env.ADMIN_GENDER,
+          role: Role.Admin,
+        },
+      ])
+      .execute();
+  }
+}

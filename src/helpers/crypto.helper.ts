7d2c4bdb24c1b91d8d7bb7d72a3b5317202e3e5d
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-24 23:09:07 -0300

    refactor(helpers): add environment variables for algorithm property and ivLength property

--- src/helpers/crypto.helper.ts
+++ src/helpers/crypto.helper.ts
@@ -2,8 +2,8 @@ import { EncryptionTransformer } from 'typeorm-encrypted';
 require('dotenv/config');
 
 export const dataEncryption = new EncryptionTransformer({
-  key: process.env.DB_KEY,
-  algorithm: 'aes-256-cbc',
-  ivLength: 16,
-  iv: process.env.DB_IV,
+  key: process.env.AES_KEY,
+  algorithm: process.env.AES_ALGORITHM,
+  ivLength: parseInt(process.env.AES_IV_LENGTH),
+  iv: process.env.AES_IV,
 });

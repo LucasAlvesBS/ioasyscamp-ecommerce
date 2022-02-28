2b8fe6e9dae8826f7e07bca349298642b62633e1
Lucas Alves <lucasalvesbs6@gmail.com>
2022-02-28 16:32:40 -0300

    refactor(helpers): change cpf and telephone messages

--- src/helpers/message.helper.ts
+++ src/helpers/message.helper.ts
@@ -4,17 +4,15 @@ export const MessageHelper = {
   FIRST_NAME_VALID:
     'firstName must be only letters, hyphen and/or single quotes',
   LAST_NAME_VALID: 'lastName must be only letters, hyphen and/or single quotes',
-  STOCK_DESCRIPTION_VALID:
-    'stock description must be only letters, hyphen and/or single quotes',
   PASSWORD_OR_EMAIL_INVALID: 'invalid credentials',
   PASSWORD_MIN_VALID: 'password must be at least 8 characters',
   PASSWORD_MAX_VALID: 'password must be a maximum of 30 characters',
-  CPF_VALID: 'cpf is invalid',
-  TELEPHONE_VALID: 'telephone is invalid',
-  STATE_VALID: 'state must be only letters',
-  CITY_VALID: 'city must be only letters',
+  CPF_VALID: 'cpf must be only numbers',
+  TELEPHONE_VALID:
+    'telephone must be only numbers. Ex.: xx9xxxxxxxx for cell phone or xxxxxxxxxx for landline',
+  STATE_VALID: 'state must be only letters hyphen and/or single quotes',
+  CITY_VALID: 'city must be only letters hyphen and/or single quotes',
   PAYMENT_VALID: 'payment must be cash, credit_card, debit_card or pix',
-  COLOR_VALID: 'color must be only letters',
   SECTION_VALID: 'section must be masculine, feminine or kids',
   ZIP_CODE_VALID: 'zipCode must be only numbers',
   INFORMATIONS_INVALID: 'invalid informations',
@@ -23,4 +21,5 @@ export const MessageHelper = {
     'Review the foreign key fields because some information is wrong',
   NOT_FOUND: 'Unfortunately, the requested parameter was not found',
   FORBIDDEN: 'You do not have sufficient privileges to access the page',
+  UNPROCESSABLE_ENTITY: 'It is necessary to put products to finalize the order',
 };
